681b0e2edce0a7e0fb7451772db781c104eb029d
Crowdbotics <team@crowdbotics.com>
2021-09-02 03:39:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "billowing_wood_30264Identifier",
+  url: "https://billowing_wood_30264Identifier.botics.co",
+  api: "https://billowing_wood_30264Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
